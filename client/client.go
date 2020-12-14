1e110076ccdfea28423b4e191769b15f22bfbb17
Evan2698 <zwh2698@163.com>
2020-12-14 20:09:37 +0800

    34

--- client/client.go
+++ client/client.go
@@ -3,12 +3,15 @@ package client
 import (
 	"context"
 	"crypto/tls"
-	"fmt"
-	"io"
+	"errors"
+	"log"
 	"net"
+	"quicvpn/common"
 	"quicvpn/config"
 	"quicvpn/tun"
+	"quicvpn/utils"
 	"strconv"
+	"sync"
 
 	"github.com/lucas-clemente/quic-go"
 )
@@ -22,33 +25,102 @@ func LauchClient(fd int, setting config.VPNSetting) {
 
 	addr := net.JoinHostPort(setting.Server, strconv.Itoa(int(setting.Port)))
 	dev, _ := tun.CreateAndroidTunDevice(fd)
-	dev.
+
+	var w sync.WaitGroup
+	var e, e1 error
+
+	for {
+		session, err := quic.DialAddr(addr, tlsConf, nil)
+		if err != nil && e == nil && e1 == nil {
+			log.Println("create session failed", err)
+			continue
+		}
+		stream, err := session.OpenStreamSync(context.Background())
+		if err != nil && e == nil && e1 == nil {
+			log.Println("open stream failed", err)
+			continue
+		}
+
+		w.Add(1)
+		go func() {
+			e = recvfromRemote(stream, dev)
+			w.Done()
+
+		}()
+
+		e1 = send2remote(stream, dev)
+		if e1.Error() != "1" && e.Error() != "1" {
+			stream.Close()
+		}
+
+		w.Wait()
+
+		if e1.Error() == "1" || e.Error() == "1" {
+			log.Println("There is a fetal error on tun device!!")
+			break
+		}
+		e1 = nil
+		e = nil
+
+		session.CloseWithError(23, "xxxx")
+	}
+
+}
+
+func recvfromRemote(s quic.Stream, dev tun.Tun) error {
+
+	buffer := make([]byte, tun.MTU)
+	for {
+
+		content, err := common.ReadXBytes(4, buffer[:4], s)
+		if err != nil {
+			log.Println("read raw content failed", err)
+			return errors.New("3")
+		}
+
+		vLen := utils.Bytes2Int(content)
+		log.Println("read: ", content, vLen)
+
+		if vLen > tun.MTU {
+			log.Println("content length is too long", vLen)
+			return errors.New("2")
+		}
+
+		buffer, err = common.ReadXBytes(vLen, buffer, s)
+		if err != nil {
+			log.Println("read content failed: ", err)
+			return errors.New("4")
+		}
+
+		_, err = dev.Write(buffer[:vLen])
+		if err != nil {
+			return errors.New("1")
+		}
+	}
+}
+
+func send2remote(s quic.Stream, dev tun.Tun) error {
 
 	for {
 
-	}
+		content, err := dev.Read()
+		if err != nil {
+			log.Println("read from tun failed!!!")
+			return errors.New("1")
+		}
 
-	session, err := quic.DialAddr(addr, tlsConf, nil)
-	if err != nil {
-		return err
-	}
+		ulen := len(content)
+		vLenBuffer := utils.Int2Bytes(uint32(ulen))
+		_, err = common.WriteXBytes(vLenBuffer, s)
+		if err != nil {
+			log.Println("write length failed!!")
+			return errors.New("2")
+		}
+		_, err = common.WriteXBytes(content, s)
+		if err != nil {
+			log.Println("write content to remote failed!!")
+			return errors.New("3")
+		}
 
-	stream, err := session.OpenStreamSync(context.Background())
-	if err != nil {
-		return err
 	}
-
-	fmt.Printf("Client: Sending '%s'\n", message)
-	_, err = stream.Write([]byte(message))
-	if err != nil {
-		return err
-	}
-
-	buf := make([]byte, len(message))
-	_, err = io.ReadFull(stream, buf)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Client: Got '%s'\n", buf)
-
 }
